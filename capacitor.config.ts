16ae4087c378f6b60c982d73f797848069c06e79
sergio20265 <psergio48@gmail.com>
2026-06-03 17:22:34 +0300

    v0.4.0: rename app to astrobuilder

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.astrositebuilder.app',
-  appName: 'Astro Site Builder',
+  appName: 'astrobuilder',
   webDir: 'dist',
 };
 
